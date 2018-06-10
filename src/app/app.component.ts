df56d2906f545164ca2a0711462b3f8aaa56f082
Dominik Kundel <dominik.kundel@gmail.com>
2018-06-10 17:32:14 +0200

    :sparkles: Set files up for talk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public currentValue = 0;
+  public submitted = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.submitted = Boolean(localStorage.getItem('voted')) || false;
+  }
+
+  async submitRating() {
+    const url =
+      'https://cors-anywhere.herokuapp.com/https://dramatic-sidewalk-3548.twil.io/vote-talk';
+    const event = 'ndcoslo-webcomponents';
+    const vote = this.currentValue;
+    // if (this.submitted) {
+    //   return;
+    // }
+
+    try {
+      const resp = await this.http.post(url, { vote, event }).toPromise();
+      console.log(resp);
+      this.submitted = true;
+      localStorage.setItem('voted', 'true');
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
